b53e99d38695a2fd74018dffd46c9c2980b61b3e
Wes Todd <wes@wesleytodd.com>
2020-02-01 12:31:15 -0800

    WIP: initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# @pkgjs/membership-updater
+
+[![NPM Version](https://img.shields.io/npm/v/@pkgjs/membership-updater.svg)](https://npmjs.org/package/@pkgjs/membership-updater)
+[![NPM Downloads](https://img.shields.io/npm/dm/@pkgjs/membership-updater.svg)](https://npmjs.org/package/@pkgjs/membership-updater)
+[![Build Status](https://travis-ci.org/wesleytodd/@pkgjs/membership-updater.svg?branch=master)](https://travis-ci.org/wesleytodd/@pkgjs/membership-updater)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](https://github.com/standard/standard)
+
+A GitHub action to manage group membership
+
+```
+$ npm i @pkgjs/membership-updater
+```
---
+++ index.js
@@ -0,0 +1,72 @@
+'use strict'
+const core = require('@actions/core')
+const github = require('@actions/github')
+const membership = require('./membership')
+
+;(async function () {
+  try {
+    const repo = github.context.repo
+    const team = core.getInput('team', { required: true })
+    const label = core.getInput('label', { required: true })
+    const filename = core.getInput('filename', { required: true })
+    // const artifact = core.getInput('artifact')
+
+    switch (github.context.eventName) {
+      case 'schedule':
+        await onSchedule(process.env.TEAM_MAINTAINER_TOKEN, {
+          ...repo,
+          team,
+          label
+        })
+        break
+      case 'pull_request':
+        await onPullRequest(process.env.GITHUB_TOKEN, {
+          ...repo,
+          number: github.context.payload.number,
+          team,
+          label,
+          filename
+       })
+      case 'push':
+      case 'issues':
+      case 'issue_comment':
+        // @TODO track user activity
+        break
+    }
+  } catch (err) {
+    console.log(err)
+    core.setFailed(err.message)
+  }
+})()
+
+async function onSchedule (token, opts) {
+  const client = new github.GitHub(token)
+  const pullsMerged = await membership.processMembership(client, {
+    owner: opts.owner,
+    repo: opts.repo,
+    team: opts.team
+  })
+  if (pullsMerged.length) {
+    console.log(`PRs Merged:
+- ${pullsMerged.map((p) => `#${p.number}: ${p.title}`).join('\n- ')}`)
+  } else {
+    console.log('No PRs merged')
+  }
+  return updatedPulls
+}
+
+async function onPullRequest (token, opts) {
+  const client = new github.GitHub(token)
+  const { data: pull } = await client.pulls.get({
+    owner: opts.owner,
+    repo: opts.repo,
+    pull_number: opts.number
+  })
+
+  // If no matching label, skip
+  if (!pull.labels.find((l) => l.name === opts.label)) {
+    return
+  }
+
+  return membership.processPR(client, pull, opts)
+}
