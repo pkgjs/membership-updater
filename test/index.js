5405400c4b8520bf83ea4ea446ea0a41f5a9b71e
Wes Todd <wes@wesleytodd.com>
2020-02-01 12:31:15 -0800

    WIP: initial commit

---
+++ test/index.js
@@ -0,0 +1,24 @@
+'use strict'
+require('dotenv').config()
+const { suite, test, before } = require('mocha')
+const assert = require('assert')
+const github = require('@actions/github')
+const pkg = require('../package.json')
+const { processMembership } = require('../membership')
+
+suite(pkg.name, () => {
+  let client
+  before(() => {
+    client = new github.GitHub(process.env.TEAM_MAINTAINER_TOKEN || process.env.GITHUB_TOKEN)
+  })
+
+  test('membership', async () => {
+    const pullsMerged = await processMembership(client, {
+      owner: 'pkgjs',
+      repo: 'membership-updater',
+      team: 'membership-updater-test-team',
+      file: 'MEMBERS.md'
+    })
+    assert(pullsMerged)
+  })
+})
